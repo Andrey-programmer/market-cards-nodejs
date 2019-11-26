29649e8a5977e4c00c9778b2836dcfb817be259c
Andrey <lak0sta1302@gmail.com>
2019-11-26 18:02:38 +0300

    Доработан функционал приложения. работает с json-файлами

--- public/app.js
+++ public/app.js
@@ -1,8 +1,12 @@
-document.querySelectorAll('.price').forEach(node => {
-    node.textContent = new Intl.NumberFormat('ru-RU', {
+const toCurrency = (price) => {
+    return new Intl.NumberFormat('ru-RU', {
         currency: 'rub',
         style: 'currency'
-    }).format(node.textContent)
+    }).format(price)
+}
+
+document.querySelectorAll('.price').forEach(node => {
+    node.textContent = toCurrency(node.textContent)
 })
 
 
@@ -12,10 +16,31 @@ if ($card) {
         if (event.target.classList.contains('js-remove')) {
             const id = event.target.dataset.id
             // console.log(id)
-            fetch('/card/remove/' + id, {
+            fetch('/card/remove/' + id, { 
                 method: 'delete'
             }).then(res => res.json()).then(card => {
-                console.log(card)
+                console.log(card.courses.length)
+                if(card.courses.length) {
+                    const html = card.courses.map(cd => {
+                        // console.log(cd)
+                        // debugger
+                        return `
+                    <tr>
+                        <td>${cd.title}</td>
+                        <td>${cd.count}</td>
+                        <td>
+                            <button class="btn btn-small js-remove" data-id="${cd.id}">Удалить</button>
+                        </td>
+                    </tr>
+                    `
+                    }).join('')
+
+                    // console.log(html)
+                    $card.querySelector('tbody').innerHTML = html
+                    $card.querySelector('.price').textContent = toCurrency(card.price)
+                } else {
+                    $card.innerHTML = '<p>Корзина пуста</p>'
+                }
             })
         }
     })
