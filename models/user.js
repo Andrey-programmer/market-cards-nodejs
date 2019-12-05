6ef26a3e90e89ad619fc182492e84b2fb99ba03d
Andrey <lak0sta1302@gmail.com>
2019-12-05 16:10:35 +0300

    связываем добавление курсов в корзину с бд на mongoDB

--- models/user.js
+++ models/user.js
@@ -27,5 +27,26 @@ const userSchema = new Schema({
     }
 })
 
+//При создании в mongoose() собственных методов обязательно используем слово function
+userSchema.methods.addToCart = function(course) {
+    const items = [...this.cart.items] //забираем данные массива
+    const index = items.findIndex(crs => {
+        return crs.courseId.toString() === course._id.toString() //Приводим id курса к строке и сравниваем
+    })
+
+    if(index >= 0) {
+        items[index].count += 1
+    } else {
+        items.push({
+            courseId: course._id,
+            count: 1
+        })
+    }
+
+    this.cart = {items}  //Заносим обновлённый массив items в модель
+
+    return this.save() // Возвращаем сохранённую схему
+}
+
 
 module.exports = model('User', userSchema)
