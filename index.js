78d663e02ccb44fbf198f90830e87f3899d64ff5
Andrey <lak0sta1302@gmail.com>
2019-12-09 19:11:45 +0300

    Разработан middleware для корзины

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const coursesRoutes = require('./routes/courses')
 const authRoutes = require('./routes/auth')
 // const User = require('./models/user')
 const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
 const MONGODB_URI = "mongodb+srv://Andrey_proogrammer:Lak0sta_1302@cluster0-t8bpi.mongodb.net/shop"
 const app = express() // Создаём сервер
 
@@ -54,6 +55,7 @@ app.use(session({
     store
 }))
 app.use(varMiddleware)
+app.use(userMiddleware)
 app.use('/', homeRoutes)
 app.use('/add',addRoutes)
 app.use('/courses', coursesRoutes)
