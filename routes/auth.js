539afa502e7503d146443ae4a2a5bfa7a55909bc
Andrey <lak0sta1302@gmail.com>
2019-12-10 13:53:13 +0300

    Добавил шифрование паролей

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express')
 const router = Router()
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
@@ -15,8 +16,10 @@ router.post('/login', async (req, res) => {
         const {email, password} = req.body
         const candidate = await User.findOne({email})
         if(candidate) {
-            
-            if(password === candidate.password) {
+            //Сравниваем два пароля
+            const equilPass = await bcrypt.compare(password, candidate.password)
+
+            if(equilPass) {
                 req.session.user = candidate
                 req.session.isAuthenticated = true  //создаем произвольное поле в сессии 
                 req.session.save(err => { // Ждем сохранения сессии а потом делаем редирект
@@ -66,12 +69,13 @@ router.get('/logout', async (req, res) => {
 router.post('/register', async (req, res) => {
     try {
         const {email, password, repeat, name} = req.body
+        const hashPassword = await bcrypt.hash(password, 10) //10 символов шифрования
 
         const candidate = await User.findOne({email})
         if(candidate) {
             res.redirect('/auth/login#register')
         } else {
-            const user = new User({email, name, password, cart: {items: []}}) //Создаем нового пользователя
+            const user = new User({email, name, password: hashPassword, cart: {items: []}}) //Создаем нового пользователя
             await user.save() //Ждём пока пользователь сохранится
             res.redirect('/auth/login#login')
         }
